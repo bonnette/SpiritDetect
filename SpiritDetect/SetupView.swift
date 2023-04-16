739b56abc56d24e2fe516d1df9452d67c2e4f04b
bonnette_l@yahoo.com <bonnette_l@yahoo.com>
2023-04-16 08:25:24 -0500

    Made the image on setup page round with a 4 point black border

--- SpiritDetect/SetupView.swift
+++ SpiritDetect/SetupView.swift
@@ -11,25 +11,26 @@ import SceneKit
 
 
 struct SetupView: View {
-    // Set up 2 arrays of ghosts and 2 display sections in order to
-    // split up the segmented button choice into rows of 3
+    // Set up 3 arrays of ghosts and 3 display sections in order to
+    // split up the segmented button choice into 3 rows of 3
     // This is to fit all of the ghosts in a segmented display
     @EnvironmentObject var setupdata: SetupData  // The selected ghost will be stored here
-    let GstSelect1 = ["GstPump", "BlueEyeGst", "plainGst"]
-    let GstSelect2 = ["GstLantern", "ScaryPumpkin", "DevilMask"]
-    let GstSelect3 = ["dirtyghost", "ghostHoldPum", "reddemon"]
+    let GstSelect1 = ["GstPump", "BlueEyeGst", "plainGst"] // top row of buttons
+    let GstSelect2 = ["GstLantern", "ScaryPumpkin", "DevilMask"] // Middle row of buttons
+    let GstSelect3 = ["dirtyghost", "ghostHoldPum", "reddemon"] // Bottom row of buttons
     var body: some View {
         
         NavigationStack {
             Form {
                 Section {
-                    // The "Picker" displays a list of ghosts defined in the "GstSelect1"
-                    // and GstSelect2 arrays above.
+                    // The "Picker" displays a list of ghosts defined in the "GstSelect1,"
+                    // GstSelect2 and GstSelect3 arrays above.
                     // When the user chooses which ghost to display. The file name for the 3D model of
                     // the ghost is placed in "$setupdata.ghostselected"
-                    // This is then made available using "@EnvironmentObject var setupdata: SetupData" to the
-                    // scanner page. So when the capture button on the scanner page is pressed the selected ghost is
+                    // This is then made available using "@EnvironmentObject var setupdata: SetupData" to
+                    // the scanner page. So when the capture button on the scanner page is displayed, the selected ghost is
                     // displayed in the Augmented reality view.
+                    
                     Picker("Ghost Menu1", selection: $setupdata.ghostselected) {
                         ForEach(GstSelect1, id: \.self) {
                             Text($0)
@@ -56,18 +57,21 @@ struct SetupView: View {
             }
             .navigationTitle("Select your Ghost")
         }
-        
+        // The spacer pushes the live image of the selected ghost to the bottom of the setup page.
         Spacer()
+        // The SceneView displays the selected ghost which is in setupdata.ghostselected
         SceneView(
-            
             scene: {
                 let holder = (setupdata.ghostselected + ".usdz")
                 let scene = SCNScene(named: holder)!
                 scene.background.contents = UIColor.gray
                 return scene
             }(),
-            options: [.autoenablesDefaultLighting,.allowsCameraControl]
+            options: [.autoenablesDefaultLighting,.allowsCameraControl] // This is needed so that the 3D image is not blacked out
             )
         .frame(width:300, height:300)
+        .cornerRadius(160) // Make the image frame round
+        .overlay(RoundedRectangle(cornerRadius: 160)
+            .stroke(Color.black, lineWidth: 4))
     }
 }
